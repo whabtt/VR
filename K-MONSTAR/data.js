564553c198e4f1b9ceaf2a8a82c472c131cabae3
whabtt <160987131+whabtt@users.noreply.github.com>
2024-04-19 11:45:42 +0800

    vr

---
+++ K-MONSTAR/data.js
@@ -0,0 +1,270 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "雙連捷運站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.051582275087707075,
+        "pitch": -0.030350272680392365,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.070683450412794,
+          "pitch": 0.24522576734991652,
+          "rotation": 7.0685834705770345,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7098980098928358,
+          "pitch": 0.2313554786123504,
+          "title": "雙連捷運站",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Arial; font-size: 15px; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">淡水信義線(紅線) R12雙連站</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "捷運站出來向後走",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.06945698043653081,
+        "pitch": -0.03794438449605053,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4509517111058443,
+          "pitch": 0.40608789843431126,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6109816978775058,
+          "pitch": 0.3774753205334562,
+          "title": "南京西路25巷 巷口",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "直走經過第一個入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08344403204328188,
+          "pitch": 0.30746353405145754,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.143347712338187,
+          "pitch": 0.29706442498301655,
+          "title": "南京西路25巷",
+          "text": "此為第一個路口，持續直走。左側為赤峰街77巷"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "直走經過第二個路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -4.20251211608047e-9,
+          "pitch": 0.42342552757607343,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1749334675662908,
+          "pitch": 0.4079583160926852,
+          "title": "南京西路25巷",
+          "text": "此為第二個路口，請繼續直走。左側為 赤峰街71巷"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "直走經過第三個路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24248604019903297,
+          "pitch": 0.4116662312921342,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.038274939542860764,
+          "pitch": 0.4083710561116014,
+          "title": "南京西路25巷",
+          "text": "此為第三個路，請繼續直走。左側為 赤峰街53巷"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "在第四個路口右轉",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.15494249481990963,
+        "pitch": 0.28074002229360673,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.06423608587117613,
+          "pitch": 0.4246364828935967,
+          "title": "南京西路25巷",
+          "text": "此為第四個路口，請在此路口左轉至 赤峰街49巷"
+        }
+      ]
+    }
+  ],
+  "name": "K-MONSTAR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ 五大/data.js
@@ -0,0 +1,222 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-m6",
+      "name": "M6出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.586544131776571,
+        "pitch": 0.033302738280648114,
+        "fov": 0.8373926018159894
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9682867410107274,
+          "pitch": 0.21788098918521115,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5286532757579865,
+          "pitch": 0.02974191730643838,
+          "title": "M6出口",
+          "text": "台北火車M6出口"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "左轉",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.295537960095965,
+        "pitch": -0.08752198106294706,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7505533493119705,
+          "pitch": -0.1063491652457973,
+          "rotation": 7.853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1135134301674157,
+          "pitch": 0.21959006092907174,
+          "title": "直走到下個路口",
+          "text": "從出口出來後左轉直走到下個路口"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "第一個路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0703498780087628,
+        "pitch": -0.08599243926110312,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.504421475490985,
+          "pitch": 0.13358174561989777,
+          "rotation": 6.283185307179586,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5136974627996373,
+          "pitch": 0.2585782579542908,
+          "title": "南陽街",
+          "text": "左轉直走後，遇到第二個路口後，左轉進入南陽街"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "直走抵達",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.986958334625335,
+        "pitch": 0.09610919682123509,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.998208529807261,
+          "pitch": 0.1182753258047633,
+          "rotation": 5.497787143782138,
+          "target": "4-360"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.118283739384948,
+          "pitch": 0.24033429896600111,
+          "title": "五大唱片行",
+          "text": "進入南陽街後，直走，五大會在左側的地下室"
+        }
+      ]
+    },
+    {
+      "id": "4-360",
+      "name": "五大360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "五大唱片行",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
