99a4d55169d0ef62a6321569294b44f66fb0b88b
whabtt <160987131+whabtt@users.noreply.github.com>
2024-05-03 12:00:05 +0800

    BT21

---
+++ BT21/data.js
@@ -0,0 +1,364 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-8",
+      "name": "明洞車站8號出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.1784912527508933,
+        "pitch": -0.1153574877352721,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4946064666060792,
+          "pitch": 0.3190799546874672,
+          "rotation": 8.63937979737193,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2538943922030512,
+          "pitch": 0.1985382700527385,
+          "title": "8號出口",
+          "text": "此為明洞車站8號出口"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "第一個路口左轉",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.8508679654945546,
+        "pitch": 0.047773507769449,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.281721835257688,
+          "pitch": 0.3031177220938801,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.336342706395449,
+          "pitch": -0.14081418793534262,
+          "title": "第一個路口",
+          "text": "從8號出口出來後直走遇到的第一個路口左轉"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "轉進第一個路口後一直直走",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.17987576984839393,
+        "pitch": 0.015175136340193518,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1385128474188413,
+          "pitch": 0.23802203364932595,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.12999660229485244,
+          "pitch": 0.3913310563234109,
+          "title": "直走",
+          "text": "經過第一個路口後，請繼續直走"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "第二個路口左轉",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.3259068967762069,
+        "pitch": 0.007587568170098535,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0037033999219566738,
+          "pitch": 0.14565906915711224,
+          "rotation": 10.995574287564278,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.021813240277690937,
+          "pitch": -0.06520300816709401,
+          "title": "第二個路口",
+          "text": "在此路口左轉"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "左轉後繼續直走",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.21727568239113282,
+        "pitch": -0.005069889795809246,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2227110616327277,
+          "pitch": 0.2003314445725337,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.06886919667273972,
+          "pitch": 0.2174496935278878,
+          "title": "直走",
+          "text": "第二個路口左轉後，繼續直走即可"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "第三個路口繼續直走不轉",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.00043261111540005004,
+          "pitch": 0.0015857687915872276,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2813270901033835,
+          "pitch": 0.17124168683995045,
+          "title": "第三個路口",
+          "text": "此路口不轉彎，請繼續直走即可"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "經過路口後繼續直走一小段",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.8670670593830856,
+        "pitch": 0.1387785181084631,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.882275133261249,
+          "pitch": 0.29649038694070384,
+          "rotation": 0,
+          "target": "7-line-friends"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8673557344821443,
+          "pitch": 0.4383575838548843,
+          "title": "直走",
+          "text": "經過第三個路口後，請繼續直走一小段路即抵達"
+        }
+      ]
+    },
+    {
+      "id": "7-line-friends",
+      "name": "抵達line friends門市",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.4008738717700027,
+        "pitch": -0.04101958828131558,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.5193571022445767,
+          "pitch": 0.20915032455785365,
+          "title": "LINE FRIENDS 明洞門市",
+          "text": "&nbsp; &nbsp; BT21系列角色為 LINE FRIENDS 及 韓團 BTS 合作自創的角色。<div>&nbsp; &nbsp;每隻角色皆為韓團BTS 的成員自己手會後，由LINE FRIENDS 公司的繪師協助修改而成。</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
